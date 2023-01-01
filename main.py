afbefaa91d6917317f36f81d30911333fe0d06cc
Johan Rex <52hands@gmail.com>
2023-01-01 18:43:40 +0100

    oh man everything is awesome

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-
+#main.py:
 def generate_output(the_input):
     #pretend this is something complicated
     return -1*the_input
 
-def read_int():
-    return int(input()) 
+def read_int(input_):
+    return int(input_()) 
 
-def main():
+def main(input_=input, print_=print):
     while True:
         try:
-            the_input = read_int()
+            the_input = read_int(input_)
         except EOFError:
             break
 
-        print(generate_output(the_input))
+        print_(generate_output(the_input))
 
 if __name__ == "__main__":
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,20 +1,46 @@
 
+#test_main.py:
 import sys
 from pathlib import Path
+
+#silly stuff in order to import the main.py module
 sys.path.insert(0, str(Path(__file__).parent.parent))
 import main
 
-def read_int_from_file(filename):
-    #let's read the file content into a list of strings that we can feed the input() function somehow. 
-    with open(filename, 'r') as f:
-        lines = [line.strip() for line in f.readlines()]
-    
-    #perhaps involve generator that yields the next line? idk...
-    for line in lines:
-        pass
-        #TODO... 
+
+class MockIO:
+    def __init__(self, input_path, output_path):
+        
+        with open(input_path, "r") as f:
+            self.input_lines = [line.strip() for line in f.readlines()]
+
+        with open(output_path, "r") as f:
+            self.output_lines = [line.strip() for line in f.readlines()]
+
+        self.input_file_index = 0
+        self.output_file_index = 0
+
+    def mock_input(self, prompt=None):
+        if self.input_file_index >= len(self.input_lines):
+            raise EOFError
+
+        line = self.input_lines[self.input_file_index]
+        self.input_file_index += 1
+        return line
+
+    def mock_print(self, *args, **kwargs):
+        if self.output_file_index >= len(self.output_lines):
+            raise EOFError
+
+        line = self.output_lines[self.output_file_index]
+        self.output_file_index += 1
+        assert line == " ".join([str(arg) for arg in args])
+
+
 
 def test_1():
-    #monkey patch perhaps?
-    main.read_int = read_int_from_file("tests/input_1.txt")
-    main.main()
+    mock_io = MockIO("tests/input_1.txt", "tests/output_1.txt")
+    main.main(mock_io.mock_input, mock_io.mock_print)
+
+if __name__ == "__main__":
+    test_1()
