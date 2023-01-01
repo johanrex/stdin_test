5ff94bcbb52e6e2b8d032d01cfb91dbca082be68
Johan Rex <52hands@gmail.com>
2023-01-01 17:41:58 +0100

    second

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,14 +5,16 @@ sys.path.insert(0, str(Path(__file__).parent.parent))
 import main
 
 def read_int_from_file(filename):
+    #let's read the file content into a list of strings that we can feed the input() function somehow. 
     with open(filename, 'r') as f:
         lines = [line.strip() for line in f.readlines()]
     
-    #perhaps involve generator that yields the next line?
+    #perhaps involve generator that yields the next line? idk...
     for line in lines:
         pass
         #TODO... 
 
 def test_1():
-    main.read_int = read_int_from_file("test1.txt")
+    #monkey patch perhaps?
+    main.read_int = read_int_from_file("tests/input_1.txt")
     main.main()
